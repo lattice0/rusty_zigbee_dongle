7553a16628b504dcab69b6a38cfd8cda8070197b
Lattice0 <lattice0@protonmail.com>
2024-09-04 01:49:58 -0300

    blink

--- examples/cc253x.rs
+++ examples/cc253x.rs
@@ -10,12 +10,12 @@ fn main() {
         let cc2531 = CC2531X::from_path(PathBuf::from("/dev/ttyACM0"), 115_200).unwrap();
         // Not all firmware versions support LED write as far as I understood
         let a = async {
-            // loop {
-            //     cc2531.set_led(LedStatus::On).await.unwrap();
-            //     std::thread::sleep(std::time::Duration::from_millis(500));
-            //     cc2531.set_led(LedStatus::Off).await.unwrap();
-            //     std::thread::sleep(std::time::Duration::from_millis(500));
-            // }
+            loop {
+                cc2531.set_led(LedStatus::On).await.unwrap();
+                std::thread::sleep(std::time::Duration::from_millis(500));
+                cc2531.set_led(LedStatus::Off).await.unwrap();
+                std::thread::sleep(std::time::Duration::from_millis(500));
+            }
         };
         let b = async {
             println!("version: {:?}", cc2531.version().await);
